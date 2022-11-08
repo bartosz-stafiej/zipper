1623b92778eba7c2b10f19d11bccfcf58da014e9
Bartosz Stafiej <bartosz.stafiej@polcode.net>
2022-11-08 09:56:16 +0100

    refactoring - preapare for setuping react

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -5,16 +5,6 @@ class AttachmentsController < ApplicationController
     @attachment = Attachment.new
   end
 
-  def create
-    @attachment = Attachment.new(params[:attachment].to_unsafe_h)
-
-    if @attachment.save
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
-
   def index
     @attachments = Attachment.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@
 
 Rails.application.routes.draw do
   mount Api::V1::Base => '/'
+
+  resources :attachments, only: %i[index new]
 end
